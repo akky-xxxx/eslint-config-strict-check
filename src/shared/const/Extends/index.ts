31edfc9b82cf550ce44b389b9834f6379bbb70cf
akky--xxxx <114.s.aki@gmail.com>
2022-11-04 15:15:53 +0900

    feat: define Extends constant

---
+++ src/shared/const/Extends/index.ts
@@ -0,0 +1,8 @@
+export const Extends = [
+  "plugin:@typescript-eslint/recommended",
+  "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  "plugin:jest/recommended",
+  "plugin:jest/style",
+  "plugin:unicorn/recommended",
+  "eslint:recommended",
+] as const
