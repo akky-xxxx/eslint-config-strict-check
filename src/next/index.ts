e74be0e0478386fd5417d3c7e7afba7f65f6a49c
akky-xxxx <114.s.aki@gmail.com>
2023-06-03 20:58:30 +0900

    feat: define minimum configs

---
+++ src/next/index.ts
@@ -0,0 +1,24 @@
+import { Extends } from "../shared/const/Extends"
+import { Plugins } from "../shared/const/Plugins"
+import { ReactExtends } from "../shared/const/ReactExtends"
+import { getConfigFullPath } from "../shared/utils/getConfigFullPath"
+
+export = {
+  plugins: [...Plugins, "strict-check"],
+
+  extends: [
+    ...Extends,
+    ...ReactExtends,
+    "next/core-web-vitals",
+    "prettier",
+    ...[
+      "../shared/config/import",
+      "../shared/config/tsdoc",
+      "../shared/config/jsx-a11y",
+      "../shared/config/react",
+      "../shared/config/unicorn",
+      "../shared/config/javascript",
+      "../shared/config/typescript",
+    ].map(getConfigFullPath(__dirname)),
+  ],
+}
---
+++ src/typescript/index.ts
@@ -0,0 +1,20 @@
+import { Extends } from "../shared/const/Extends"
+import { Plugins } from "../shared/const/Plugins"
+import { getConfigFullPath } from "../shared/utils/getConfigFullPath"
+
+export = {
+  plugins: Plugins,
+
+  extends: [
+    ...Extends,
+    "airbnb-base",
+    "prettier",
+    ...[
+      "../shared/config/import",
+      "../shared/config/tsdoc",
+      "../shared/config/unicorn",
+      "../shared/config/javascript",
+      "../shared/config/typescript",
+    ].map(getConfigFullPath(__dirname)),
+  ],
+}
