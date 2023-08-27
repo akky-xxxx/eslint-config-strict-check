239834762abe001e380d9c386975e36bb0d02c1c
akky-xxxx <114.s.aki@gmail.com>
2023-08-27 18:01:44 +0900

    feat: remove configs of deleted rules

--- config/eslint/typescript/configs/no_implicit_any_catch/index.js
+++
@@ -1,2 +0,0 @@
-const { Error } = require("../../../const")
-exports.no_implicit_any_catch = [Error, { allowExplicitAny: false }]
--- src/shared/config/typescript/options/noImplicitAnyCatch/index.ts
+++
@@ -1,3 +0,0 @@
-import { Severity } from "../../../../const/Severity"
-
-export const noImplicitAnyCatch = [Severity.ERROR, { allowExplicitAny: false }]
