12825f7ccfeaa4da008e212d820e7a3bccee1814
akky--xxxx <114.s.aki@gmail.com>
2023-09-14 14:28:36 +0900

    feat: apply to eslint-typescript v6
    https://zenn.dev/cybozu_frontend/articles/ts-eslint-v6-guide

--- src/shared/config/typescript/index.ts
+++ src/shared/config/typescript/index.ts
@@ -13,6 +13,7 @@ export = {
     "no-magic-numbers": Severity.OFF,
     "no-shadow": Severity.OFF,
 
+    "@typescript-eslint/adjacent-overload-signatures": Severity.ERROR,
     "@typescript-eslint/array-type": arrayType,
     "@typescript-eslint/consistent-indexed-object-style": [
       Severity.ERROR,
@@ -24,15 +25,20 @@ export = {
     "@typescript-eslint/explicit-module-boundary-types": Severity.OFF,
     "@typescript-eslint/no-confusing-void-expression":
       noConfusingVoidExpression,
+    "@typescript-eslint/no-empty-function": Severity.ERROR,
+    "@typescript-eslint/no-empty-interface": Severity.WARN,
     "@typescript-eslint/no-floating-promises": Severity.WARN,
+    "@typescript-eslint/no-inferrable-types": Severity.WARN,
     "@typescript-eslint/no-magic-numbers": Severity.WARN,
     "@typescript-eslint/no-misused-promises": Severity.WARN,
+    "@typescript-eslint/no-non-null-assertion": Severity.ERROR,
     "@typescript-eslint/no-shadow": Severity.WARN,
     "@typescript-eslint/no-unnecessary-boolean-literal-compare":
       noUnnecessaryBooleanLiteralCompare,
     "@typescript-eslint/no-unnecessary-condition": noUnnecessaryCondition,
     "@typescript-eslint/no-unnecessary-type-arguments": Severity.WARN,
     "@typescript-eslint/no-use-before-define": Severity.ERROR,
+    "@typescript-eslint/prefer-namespace-keyword": Severity.ERROR,
     "@typescript-eslint/prefer-string-starts-ends-with": Severity.WARN,
     "@typescript-eslint/sort-type-constituents": Severity.ERROR,
   },
