af93a03fc05e0918904001346aaa904acfdd20ca
akky-xxxx <114.s.aki@gmail.com>
2024-05-26 20:06:05 +0900

    feat: redefine configs for @stylistic/eslint-plugin, for flat config

--- src/shared/config/baseRules/index.ts
+++ src/shared/config/baseRules/index.ts
@@ -1,7 +1,9 @@
 import { eslintRules } from "./eslintRules"
 import { importRules } from "./importRules"
+import { stylisticRules } from "./stylisticRules"
 
 export const baseRules = {
   ...eslintRules,
   ...importRules,
+  ...stylisticRules,
 } as const
