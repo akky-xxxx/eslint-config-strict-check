8c00d69f0d76debba7099a63b9678b2869282281
akky-xxxx <114.s.aki@gmail.com>
2024-05-26 22:47:52 +0900

    feat: update entry file of this package

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-import config from "./typescriptMax"
+import { legacy } from "./legacy"
+import { typescriptFlatConfig } from "./typescript"
 
-export = config
+import type { ESLint } from "eslint"
+
+const plugin = {
+  meta: {
+    name: "eslint-config-strict-check",
+  },
+
+  configs: {
+    "legacy-typescript": legacy.typescript,
+    "typescript": typescriptFlatConfig,
+  },
+} as const satisfies ESLint.Plugin
+
+export default plugin
