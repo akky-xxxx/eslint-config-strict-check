eeddaa71939eb31145e556e78036fb58883ce486
akky-xxxx <114.s.aki@gmail.com>
2024-05-26 21:17:27 +0900

    feat: update EslintRules
    change to Linter.RulesRecord

--- src/shared/types/EslintRules/index.ts
+++ src/shared/types/EslintRules/index.ts
@@ -1,4 +1,3 @@
 import type { Linter } from "eslint"
 
-export type EslintRules =
-  Record<string, Linter.RuleEntry | readonly [Linter.RuleLevel, ...readonly unknown[]]>
+export type EslintRules = Linter.RulesRecord
