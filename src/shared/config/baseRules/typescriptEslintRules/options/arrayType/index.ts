b710cdfa6bc55b43a73f064b8c650d90ef2af872
akky-xxxx <114.s.aki@gmail.com>
2024-05-26 21:25:58 +0900

    refactor: add line break

--- src/shared/config/baseRules/typescriptEslintRules/options/arrayType/index.ts
+++ src/shared/config/baseRules/typescriptEslintRules/options/arrayType/index.ts
@@ -2,4 +2,5 @@ import { Severity } from "../../../../../const/Severity"
 
 import type { EslintRuleLevelAndOptions } from "../../../../../types/EslintRuleLevelAndOptions"
 
-export const arrayType = [Severity.ERROR, { default: "array" }] as const satisfies EslintRuleLevelAndOptions
+export const arrayType =
+  [Severity.ERROR, { default: "array" }] as const satisfies EslintRuleLevelAndOptions
