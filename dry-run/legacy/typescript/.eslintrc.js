7d5bf4ffdb04c8fe3b0b9e090ea626fd73fa9523
akky-xxxx <114.s.aki@gmail.com>
2024-05-27 00:46:14 +0900

    test: remove unnecessary property

--- dry-run/legacy/typescript/.eslintrc.js
+++ dry-run/legacy/typescript/.eslintrc.js
@@ -3,9 +3,6 @@ const { typescript } = require("../../../out/legacy/typescript")
 module.exports = {
   ...typescript,
   root: true,
-  env: {
-    node: true,
-  },
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: ["./tsconfig.json"],
