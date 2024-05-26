00d397c69c9b29327dcd958500c745dc757742ac
akky-xxxx <114.s.aki@gmail.com>
2024-05-26 15:41:08 +0900

    test: adjust lint target test file

--- dry-run/typescript/index.test.ts
+++ dry-run/typescript/index.test.ts
@@ -1,6 +1,16 @@
+// eslint-disable-next-line jest/consistent-test-it
+test("test1", () => {
+  expect(1).toBe(1)
+})
+
 describe("dry-run", () => {
+  // eslint-disable-next-line jest/consistent-test-it
+  test("test2", () => {
+    expect(1).toBe(1)
+  })
+
   // eslint-disable-next-line jest/consistent-test-it, jest/no-disabled-tests
-  test.skip("it is \"it\" instead \"test\"", () => {
+  test.skip("test3", () => {
     expect(1).toBe(1)
   })
 })
