e87affeabeed7aafda241de6490eb29474f042ca
akky-xxxx <114.s.aki@gmail.com>
2024-05-28 00:19:40 +0900

    refactor: extends react to reactMax

--- src/legacy/reactMax/index.ts
+++ src/legacy/reactMax/index.ts
@@ -1,9 +1,8 @@
-import { baseRules } from "../../shared/config/baseRules"
-import { reactRules } from "../../shared/config/reactRules"
 import { reactTestRules } from "../../shared/config/reactTestRules"
 import { storyRules } from "../../shared/config/storyRules"
 import { testRules } from "../../shared/config/testRules"
 import { FilePatterns } from "../../shared/const/FilePatterns"
+import { react } from "../react"
 
 import type { EslintLegacyConfig } from "../../shared/types/EslintLegacyConfig"
 
@@ -15,15 +14,7 @@ export const reactMax = {
     "plugin:storybook/csf",
     "plugin:storybook/csf-strict",
     "plugin:storybook/recommended",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:unicorn/recommended",
-    "eslint:recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react/recommended",
-    "plugin:strict-check/legacy-react",
-    "airbnb",
-    "plugin:react-hooks/recommended",
-    "airbnb/hooks",
+    ...react.extends,
   ],
   overrides: [
     {
@@ -41,17 +32,11 @@ export const reactMax = {
     },
   ],
   plugins: [
-    "@stylistic",
-    "@typescript-eslint",
+    ...react.plugins,
     "jest",
-    "react",
-    "react-hooks",
     "storybook",
-    "strict-check",
-    "unicorn",
   ],
   rules: {
-    ...baseRules,
-    ...reactRules,
+    ...react.rules,
   },
 } satisfies EslintLegacyConfig
