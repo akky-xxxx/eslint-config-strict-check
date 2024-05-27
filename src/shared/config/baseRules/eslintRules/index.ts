7e88f3fec737c19517e09461f5f04d993df24027
akky-xxxx <114.s.aki@gmail.com>
2024-05-28 01:02:47 +0900

    refactor: add comments

--- src/shared/config/baseRules/eslintRules/index.ts
+++ src/shared/config/baseRules/eslintRules/index.ts
@@ -6,7 +6,7 @@ export const eslintRules = {
   /* eslint-disable @typescript-eslint/no-magic-numbers */
   "complexity": [Severity.ERROR, 5],
   "max-depth": [Severity.ERROR, 3],
-  "max-len": Severity.OFF, // fix by prettier
+  "max-len": Severity.OFF, // check by stylistic
   "max-lines": [Severity.ERROR, { max: 100 }],
   "max-nested-callbacks": [Severity.ERROR, 3],
   "max-statements": [Severity.ERROR, 10],
@@ -15,9 +15,9 @@ export const eslintRules = {
   "no-undefined": Severity.WARN, // TODO: off にする
   "no-unused-vars": Severity.OFF, // check by tsc
   "no-use-before-define": Severity.OFF, // check by typescript-eslint
-  "object-curly-newline": Severity.OFF,
-  "quotes": Severity.OFF,
-  "semi": Severity.OFF,
+  "object-curly-newline": Severity.OFF, // check by stylistic
+  "quotes": Severity.OFF, // check by stylistic
+  "semi": Severity.OFF, // check by stylistic
   "sort-keys": [Severity.WARN, "asc", { allowLineSeparatedGroups: true }],
   /* eslint-enable @typescript-eslint/no-magic-numbers */
 } as const satisfies EslintRules
