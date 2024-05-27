acee4d69d3c1600cc0b6027a980530a93707e596
akky-xxxx <114.s.aki@gmail.com>
2024-05-28 00:39:37 +0900

    refactor: extends typescript to react

--- src/legacy/react/index.ts
+++ src/legacy/react/index.ts
@@ -1,30 +1,25 @@
-import { baseRules } from "../../shared/config/baseRules"
 import { reactRules } from "../../shared/config/reactRules"
+import { typescript } from "../typescript"
 
 import type { EslintLegacyConfig } from "../../shared/types/EslintLegacyConfig"
 
 export const react = {
   extends: [
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:unicorn/recommended",
-    "eslint:recommended",
+    ...typescript.extends,
     "plugin:react/jsx-runtime",
     "plugin:react/recommended",
     "plugin:strict-check/legacy-react",
     "airbnb",
-    "plugin:react-hooks/recommended",
     "airbnb/hooks",
   ],
   plugins: [
-    "@stylistic",
-    "@typescript-eslint",
+    ...typescript.plugins,
     "react",
     "react-hooks",
     "strict-check",
-    "unicorn",
   ],
   rules: {
-    ...baseRules,
+    ...typescript.rules,
     ...reactRules,
   },
 } satisfies EslintLegacyConfig
