58a7b5fc3713b378609c006e6419626ffec01092
akky-xxxx <114.s.aki@gmail.com>
2024-05-29 01:30:04 +0900

    feat: comment out next relation

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { legacy } from "./legacy"
-import { nextFlatConfig } from "./next"
-import { nextMaxFlatConfig } from "./nextMax"
+// TODO: config-next を適用させる
+//  読み込ませると下記のエラーが出る
+//  Failed to patch ESLint because the calling module was not recognized.
+// import { nextFlatConfig } from "./next"
+// import { nextMaxFlatConfig } from "./nextMax"
 import { reactFlatConfig } from "./react"
 import { reactMaxFlatConfig } from "./reactMax"
 import { typescriptFlatConfig } from "./typescript"
@@ -20,8 +23,8 @@ const plugin = {
     "legacy-react-max": legacy.reactMax,
     "legacy-typescript": legacy.typescript,
     "legacy-typescript-max": legacy.typescriptMax,
-    "next": nextFlatConfig,
-    "next-max": nextMaxFlatConfig,
+    // "next": nextFlatConfig,
+    // "next-max": nextMaxFlatConfig,
     "react": reactFlatConfig,
     "react-max": reactMaxFlatConfig,
     "typescript": typescriptFlatConfig,
