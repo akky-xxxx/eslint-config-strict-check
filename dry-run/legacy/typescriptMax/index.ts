3a45fd84aeab352395242141b9eebe1cfc1ffb29
akky-xxxx <114.s.aki@gmail.com>
2024-05-29 15:35:59 +0900

    test: remove unnecessary disable directives

--- dry-run/legacy/typescriptMax/index.ts
+++ dry-run/legacy/typescriptMax/index.ts
@@ -143,7 +143,6 @@ export const javascriptObjectTest = {
   one: "one",
 
   five: "five",
-  // eslint-disable-next-line @stylistic/quotes
   four: 'four',
   // eslint-disable-next-line no-undefined
   six: undefined,
